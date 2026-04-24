4ec5056a41a06e2fc49394a31a797180af14d13e
Auanny <auannycomerlato@alunos.utfpr.edu.br>
2026-04-24 20:39:09 -0300

    teste commit

---
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.pb.pw44s.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.utfpr.pb.pw44s.server.model.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
